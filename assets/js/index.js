bb41d3714d993029f3f11b843b0ab8519830f5e4
KitagawaHiyori <w.d.f.hiyori@gmail.com>
2022-09-09 19:05:46 +0900

    Wordpress と連携済み

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+
+
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+
+
+
+
+$('.slider').slick({
+		fade:true,//切り替えをフェードで行う。初期値はfalse。
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+		speed:1000,//スライドの動きのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 1,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+		arrows: true,//左右の矢印あり
+
+		prevArrow: '<div class="slick-prev slick-arrow"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/prev-arrow.svg" alt=""></div>',//矢印部分PreviewのHTMLを変更
+		nextArrow: '<div class="slick-next slick-arrow"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/next-arrow.svg" alt=""></div>',//矢印部分NextのHTMLを変更
+		dots: true,//下部ドットナビゲーションの表示
+        dotsClass: 'slick-dots',
+        pauseOnFocus: false,//フォーカスで一時停止を無効
+        pauseOnHover: false,//マウスホバーで一時停止を無効
+        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
