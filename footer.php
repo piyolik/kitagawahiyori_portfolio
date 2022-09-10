103be7a64185425cf7949f64135dfa12b87157ec
KitagawaHiyori <w.d.f.hiyori@gmail.com>
2022-09-10 19:38:26 +0900

    footerまで作り込み完了

--- footer.php
+++ footer.php
@@ -1,16 +1,17 @@
+<!--========STEP1-4 footer.php を作り込む ========-->
+
 <footer>
         
     </footer>
 
-    <!-- Slickのライブラリ -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
     <script src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/js/index.js"></script>
 
-
-
+<!-- ①　Body終了タグ直前に記述 -->
+<?php wp_footer();?>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -1,36 +1,52 @@
+<!--========STEP1-3 header.php を作り込む ========-->
+
 <!DOCTYPE html>
-<html lang="ja">
+
+<!-- ①　サイトの言語属性を出力する -->
+<html <?php language_attributes(); ?> >
+
+
 
 <head>
-    <meta charset="UTF-8">
+    <!-- ②　文字コードを出力する -->
+    <meta charset="<?php bloginfo('charset')?>">
+
+    <!-- ③　キャッチフレーズを出力する -->
+    <meta name="description" content="<?php bloginfo('description');?>">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
     <link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/css/theme.css">
     
-    <!-- font のソースコードを挿入-->
+    
 
     <title>TOP</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
 
-    <!-- モリサワフォント -->
     <script type="text/javascript"
         src="//typesquare.com/3/tsst/script/ja/typesquare.js?5ff51e8ec9f0470690204792e90393a3" charset="utf-8"></script>
 
+<!-- ⑥　head終了タグ直前に記述 -->
+<?php wp_head();?>
 </head>
 
-<body>
+<!-- ⑤　bodytタグのクラスを動的に出力する -->
+<body <?php body_class();?>>
+
+    <?php wp_body_open();?>
+    <!-- ⑦　bodyの開始タグ直後に記述 -->
     <header class="HeaderPC pc">
         <div class="HeaderPC__Inner inner">
-
-
 
             <h1 class="HeaderPC__title">
                 <div class="HeaderPC__title__title-copy">
                     <p>キタガワ ヒヨリ</p>
                     <div class="HeaderPC__title__logo">
-                        <a class="" href="#">
-                            <!-- ① ロゴ写真を変更 ----- -->
-                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/hiyori.GIF" alt="sand the fruit and...">
+                        <a class="" href="<?php echo esc_url(home_url());?>">
+                            <!-- ④　ロゴのリンクと代替コードを書き換える -->
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/hiyori.GIF" alt="<?php bloginfo('name');?>">
 
                         </a>
                     </div>
