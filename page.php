c3eea1f65614f983538c8011ed3139c72a13dc17
KitagawaHiyori <w.d.f.hiyori@gmail.com>
2022-09-10 21:05:43 +0900

    page作成

---
+++ page.php
@@ -0,0 +1,71 @@
+<?php get_header();?>
+
+    <main class="Works">
+        <section class="WorksList">
+            <div class="WorksList__inner">
+                <ul class="WorksList__list">
+
+                    <li class="WorksList__item">
+                        <a href="#">
+                            <div class="WorkList__img">
+                                <img src="img/1-sp-mukohan.png" alt="">
+                            </div>
+                            <p class="WorkList__title">【 ロゴ 】無人古着販売店「MUKOHAN」
+
+                            </p>
+                        </a>
+
+                    </li>
+                    <li class="WorksList__item">
+                        <a href="#">
+                            <div class="WorkList__img">
+                                <img src="img/2-sp-fakesity_app1-min.png" alt="">
+                            </div>
+                            <p class="WorkList__title">【 アプリUI 】マイノリティのための架空のSNS『Fakesity』
+                            </p>
+                        </a>
+
+                    </li>
+                    <li class="WorksList__item">
+                        <a href="#">
+                            <div class="WorkList__img">
+                                <img src="img/1-sp-FancelBanner.png" alt="">
+                            </div>
+                            <p class="WorkList__title">【 SNSバナー 】25歳〜30歳女性をターゲットにした架空の広告バナー
+
+                            </p>
+                        </a>
+
+                    </li>
+                    <li class="WorksList__item">
+                        <a href="#">
+                            <div class="WorkList__img">
+                                <img src="img/1-sp-Flamingo-min.png" alt="">
+                            </div>
+                            <p class="WorkList__title">【 ポスター 】天王寺動物園の園内マップ兼広告ツール
+
+                            </p>
+                        </a>
+
+                    </li>
+                    <li class="WorksList__item">
+                        <a href="#">
+                            <div class="WorkList__img">
+                                <img src="img/1-sp-nippon-min.png" alt="">
+                            </div>
+                            <p class="WorkList__title">【 PV 】東京オリンピックのプロモーションビデオ
+
+                            </p>
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+
+        </section>
+
+
+
+    </main>
+
+<?php get_header();?>
