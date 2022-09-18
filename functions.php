be577bc27cf9320ef81b3452cdf1e1ad342bb7de
KitagawaHiyori <w.d.f.hiyori@gmail.com>
2022-09-18 17:09:40 +0900

    a

--- functions.php
+++ functions.php
@@ -1,13 +1,14 @@
 <!--========STEP1-5 ページごとに、ページタイトルを正しく表示させる ========-->
 
 
-<!-- ① 関数「piyofolio_theme_setup()」を定義する （ ページタイトル機能を有効化させる　）-->
+<!-- ① 関数「piyofolio_theme_setup()」を定義する （ ページタイトル機能を有効化させる）-->
 <?php function piyofolio_theme_setup()
 {
   add_theme_support('title-tag');
 }
 
-
-// ② 関数「piyofolio_theme_setup()」を「after_setup_theme」のタイミングに実行させる
 add_action('after_setup_theme', 'piyofolio_theme_setup');
 ?>
+
+
+<!-- ② 関数「piyofolio_theme_setup()」を「after_setup_theme」のタイミングに実行させる -->
