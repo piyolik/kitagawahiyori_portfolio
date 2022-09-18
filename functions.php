7d2f309fe44e3be40e042bf514767588851b1323
KitagawaHiyori <w.d.f.hiyori@gmail.com>
2022-09-18 17:34:44 +0900

    画像がアップロードできない問題解消

--- functions.php
+++ functions.php
@@ -1,14 +1,6 @@
-<!--========STEP1-5 ページごとに、ページタイトルを正しく表示させる ========-->
-
-
-<!-- ① 関数「piyofolio_theme_setup()」を定義する （ ページタイトル機能を有効化させる）-->
-<?php function piyofolio_theme_setup()
+<?php
+function setup_my_theme()
 {
   add_theme_support('title-tag');
 }
-
-add_action('after_setup_theme', 'piyofolio_theme_setup');
-?>
-
-
-<!-- ② 関数「piyofolio_theme_setup()」を「after_setup_theme」のタイミングに実行させる -->
+add_action('after_setup_theme', 'setup_my_theme');
